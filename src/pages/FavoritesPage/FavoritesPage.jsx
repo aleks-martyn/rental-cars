d0f3db1a08c48967891cbcdbd2cf5afbbcf7ea8a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-29 23:19:52 +0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,41 +1,39 @@
-import { useState, useEffect } from "react";
-import { fetchAllCars } from "../../services/api";
-import { CarList } from "../../components/CarList";
-import { getFavoriteCars } from "../../js/getFavoriteCars";
-import { LoadmoreBtn } from "../../components/LoadmoreBtn";
-import { Spinner } from "../../components/Loader";
+import { useState, useEffect } from 'react';
+import { fetchAllCars } from '../../services/api';
+import { CarList } from '../../components/CarList';
+import { getFavoriteCars } from '../../js/getFavoriteCars';
+import { LoadmoreBtn } from '../../components/LoadmoreBtn';
+import { Spinner } from '../../components/Loader';
 
 export default function FavoritesPage() {
   const [favoriteCars, setFavoriteCars] = useState([]);
   const [error, setError] = useState(null);
-  const [status, setStatus] = useState("pending");
-  const LOCALSTORAGE_KEY = "favorite-cars";
+  const [status, setStatus] = useState('pending');
+  const LOCALSTORAGE_KEY = 'favorite-cars';
 
   useEffect(() => {
-    const getCars = async () => {
-      try {
-        const res = await fetchAllCars();
+    fetchAllCars()
+      .then(res => {
         const items = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-        const ids = items?.map((item) => item.id);
+        const ids = items?.map(item => item.id);
 
         setFavoriteCars(getFavoriteCars(ids, res));
-        setStatus("resolved");
-      } catch (error) {
+        setError(null);
+        setStatus('resolved');
+      })
+      .catch(error => {
         setError(error);
-        setStatus("rejected");
-      }
-    };
-
-    getCars();
+        setStatus('rejected');
+      });
   }, []);
 
-  const handleLoadmoreBtnClick = () => console.log("Under development)");
+  const handleLoadmoreBtnClick = () => console.log('Under development)');
 
   return (
     <>
-      {status === "pending" && <Spinner />}
-      {status === "rejected" && <h3>{error.message}</h3>}
-      {status === "resolved" && <CarList cars={favoriteCars} />}
+      {status === 'pending' && <Spinner />}
+      {status === 'rejected' && <h3>{error.message}</h3>}
+      {status === 'resolved' && <CarList cars={favoriteCars} />}
       <LoadmoreBtn onClick={handleLoadmoreBtnClick} />
     </>
   );
