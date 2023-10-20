de8f94a70f50b6d7dc537c20a8dcad84fe3c4bc5
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-10-20 17:05:41 +0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react';
-import { fetchAllCars } from '../../services/api';
+import { fetchAllCars } from 'services/api';
 import { CarList } from 'components/CarList';
-import { getFavoriteCars } from '../../js/getFavoriteCars';
+import { getFavoriteCars } from 'js/getFavoriteCars';
+import { load, save } from 'js/localStorageFunctions';
 import { LoadmoreBtn } from 'components/LoadmoreBtn';
 import { Spinner } from 'components/Loader';
 import { DropdownMenu } from 'components/DropdownMenu';
+import { LOCALSTORAGE_KEY } from 'constants';
 
 export default function FavoritesPage() {
   const [allCars, setAllCars] = useState([]);
   const [favoriteCars, setFavoriteCars] = useState([]);
   const [error, setError] = useState(null);
   const [status, setStatus] = useState('pending');
-  const LOCALSTORAGE_KEY = 'favorite-cars';
 
   useEffect(() => {
     fetchAllCars()
@@ -27,10 +28,7 @@ export default function FavoritesPage() {
   }, []);
 
   useEffect(() => {
-    const storedFavorites =
-      JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? [];
-
-    const ids = storedFavorites?.map(item => item.id);
+    const ids = load(LOCALSTORAGE_KEY).map(item => item.id);
 
     setFavoriteCars(getFavoriteCars(ids, allCars));
     setError(null);
@@ -40,8 +38,7 @@ export default function FavoritesPage() {
   const handleLoadmoreBtnClick = () => console.log('Under development');
 
   const toggleFavorite = carId => {
-    const storedFavorites =
-      JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? [];
+    const storedFavorites = load(LOCALSTORAGE_KEY);
 
     const isFavorite = storedFavorites?.find(item => item.id === carId);
 
@@ -49,7 +46,7 @@ export default function FavoritesPage() {
       const index = storedFavorites?.findIndex(item => item.id === carId);
 
       if (index !== -1) storedFavorites?.splice(index, 1);
-      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storedFavorites));
+      save(LOCALSTORAGE_KEY, storedFavorites);
       const ids = storedFavorites?.map(item => item.id);
 
       setFavoriteCars(getFavoriteCars(ids, allCars));
