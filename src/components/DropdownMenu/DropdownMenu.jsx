a895fe784f5035a40716e209d0c72ecc006d7d46
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-10-31 23:40:11 +0200

    Update DropdownMenu.jsx

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -1,13 +1,50 @@
 import { CustomSelect } from 'components/CustomSelect';
 import { prices } from 'constants';
-import { Wrap } from './DropdownMenu.styled';
+import {
+  Wrap,
+  InputOuterWrap,
+  Subtitle,
+  InputWrap,
+  InputInnerWrap,
+  StyledLabel,
+  StyledInput,
+  FormBtn,
+} from './DropdownMenu.styled';
 
-export const DropdownMenu = ({ brands }) => {
+export const DropdownMenu = ({ brands, onBrandChange, onPriceChange }) => {
   return (
     <Wrap>
-      <CustomSelect title="Car brand" text="Enter the text" items={brands} />
+      <CustomSelect
+        title="Car brand"
+        text="Enter the text"
+        items={brands}
+        onValueChange={onBrandChange}
+      />
 
-      <CustomSelect title="Price / 1 hour" text="To $" items={prices} />
+      <CustomSelect
+        title="Price / 1 hour"
+        text="To $"
+        items={prices}
+        onValueChange={onPriceChange}
+      />
+
+      <InputOuterWrap>
+        <Subtitle>Car mileage / km</Subtitle>
+
+        <InputWrap>
+          <InputInnerWrap>
+            <StyledLabel htmlFor="from">From</StyledLabel>
+            <StyledInput type="number" min={0} name="from" id="from" />
+          </InputInnerWrap>
+
+          <InputInnerWrap>
+            <StyledLabel htmlFor="to">To</StyledLabel>
+            <StyledInput type="number" min={0} name="to" id="to" />
+          </InputInnerWrap>
+        </InputWrap>
+      </InputOuterWrap>
+
+      <FormBtn type="button">Reset</FormBtn>
     </Wrap>
   );
 };
