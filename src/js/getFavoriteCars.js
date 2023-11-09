8bd55c30a8931d851e887f189b5d29b855bbff9f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-11-09 20:30:52 +0200

    Update getFavoriteCars.js

--- src/js/getFavoriteCars.js
+++ src/js/getFavoriteCars.js
@@ -1,12 +1,12 @@
 export const getFavoriteCars = (carIds, cars) => {
-  if (!cars || !carIds || carIds.length === 0 || cars.length === 0) return;
+  if (!cars || !carIds || carIds.length === 0 || cars.length === 0) return [];
 
-  const filteredCars = [];
+  const favoriteCars = [];
 
   for (const car of cars) {
     if (carIds.includes(car.id)) {
-      filteredCars.push(car);
+      favoriteCars.push(car);
     }
   }
-  return filteredCars;
+  return favoriteCars;
 };
