6de10222df5fc4ab7835e3aec165c91c2b933363
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-11-18 22:09:50 +0200

    Create getFilteredCars.js

---
+++ src/js/getFilteredCars.js
@@ -0,0 +1,33 @@
+import { getModifiedPrice } from './getModifiedPrice';
+import { getModifiedMileage } from './getModifiedMileage';
+
+export const getFilteredCars = (
+  cars,
+  { brand, price, minMileage, maxMileage }
+) => {
+  const filteredCars = cars.filter(({ make, rentalPrice, mileage }) => {
+    const modifiedPrice = getModifiedPrice(rentalPrice);
+    const modifiedMinMileage = getModifiedMileage(minMileage);
+    const modifiedMaxMileage = getModifiedMileage(maxMileage);
+
+    if (brand !== 'Enter the text' && make !== brand) {
+      return false;
+    }
+
+    if (price !== 'To $' && modifiedPrice > price) {
+      return false;
+    }
+
+    if (modifiedMinMileage !== 0 && modifiedMinMileage > mileage) {
+      return false;
+    }
+
+    if (modifiedMaxMileage !== 0 && modifiedMaxMileage < mileage) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return filteredCars;
+};
