08c87c1a03eb5d13049f137b39c5e1057a6ae57a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-11-18 22:12:56 +0200

    Update CatalogPage.jsx

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,18 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { debounce } from 'lodash';
 import { fetchCars } from 'services/api';
 import { getUniqueBrands } from 'js/getUniqueBrands';
-import { load, save } from 'js/localStorageFunctions';
+import { getFilteredCars } from 'js/getFilteredCars';
+import { load, save, remove } from 'js/localStorageFunctions';
 import { CarList } from 'components/CarList';
 import { LoadmoreBtn } from 'components/LoadmoreBtn';
 import { Spinner } from 'components/Loader';
-import { DropdownFilter } from 'components/DropdownFilter';
-import { FAVORITES_KEY } from 'constants';
+import { DropdownMenu } from 'components/DropdownMenu';
+import {
+  FAVORITES_KEY,
+  CATALOG_BRAND,
+  CATALOG_PRICE,
+  CATALOG_MIN_MILEAGE,
+  CATALOG_MAX_MILEAGE,
+} from 'constants';
 
 export default function CatalogPage() {
   const [cars, setCars] = useState([]);
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
   const [status, setStatus] = useState('pending');
+  const [selectedBrand, setSelectedBrand] = useState('Enter the text');
+  const [selectedPrice, setSelectedPrice] = useState('To $');
+  const [minMileage, setMinMileage] = useState('');
+  const [maxMileage, setMaxMileage] = useState('');
+  const [filterMinMileage, setFilterMinMileage] = useState('');
+  const [filterMaxMileage, setFilterMaxMileage] = useState('');
+  const [filteredCars, setFilteredCars] = useState([]);
+
+  const debounsedMinMileage = useRef(
+    debounce(value => setFilterMinMileage(value), 300)
+  ).current;
+
+  const debounsedMaxMileage = useRef(
+    debounce(value => setFilterMaxMileage(value), 300)
+  ).current;
 
   useEffect(() => {
     fetchCars(page)
@@ -26,6 +49,50 @@ export default function CatalogPage() {
         setStatus('rejected');
       });
   }, [page]);
+
+  useEffect(() => {
+    const savedBrand = load(CATALOG_BRAND);
+    if (savedBrand) {
+      setSelectedBrand(savedBrand);
+    }
+
+    const savedPrice = load(CATALOG_PRICE);
+    if (savedPrice) {
+      setSelectedPrice(savedPrice);
+    }
+
+    const savedMinMileage = load(CATALOG_MIN_MILEAGE);
+    if (savedMinMileage) {
+      setMinMileage(savedMinMileage);
+      setFilterMinMileage(savedMinMileage);
+    }
+
+    const savedMaxMileage = load(CATALOG_MAX_MILEAGE);
+    if (savedMaxMileage) {
+      setMaxMileage(savedMaxMileage);
+      setFilterMaxMileage(savedMaxMileage);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (cars.length === 0) setFilteredCars([]);
+
+    const filter = {
+      brand: selectedBrand,
+      price: selectedPrice,
+      minMileage: filterMinMileage,
+      maxMileage: filterMaxMileage,
+    };
+
+    setFilteredCars(getFilteredCars(cars, filter));
+  }, [cars, selectedBrand, selectedPrice, filterMinMileage, filterMaxMileage]);
+
+  useEffect(() => {
+    return () => {
+      debounsedMinMileage.cancel();
+      debounsedMaxMileage.cancel();
+    };
+  }, [debounsedMinMileage, debounsedMaxMileage]);
 
   const toggleFavorite = id => {
     const storedFavoritesIds = load(FAVORITES_KEY) ?? [];
@@ -45,27 +112,64 @@ export default function CatalogPage() {
     }
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const brand = form.cars.value.trim();
-    if (brand) console.log(brand);
-    const price = form.prices.value;
-    if (price) console.log(price);
-    const from = form.from.value;
-    if (from) console.log(from);
-    const to = form.to.value;
-    if (to) console.log(to);
-    form.reset();
+  const handleChange = (id, value) => {
+    switch (id) {
+      case 'brand':
+        setSelectedBrand(value);
+        save(CATALOG_BRAND, value);
+        break;
+
+      case 'price':
+        setSelectedPrice(Number(value));
+        save(CATALOG_PRICE, Number(value));
+        break;
+
+      case 'min':
+        setMinMileage(value);
+        debounsedMinMileage(value);
+        save(CATALOG_MIN_MILEAGE, value);
+        break;
+
+      case 'max':
+        setMaxMileage(value);
+        debounsedMaxMileage(value);
+        save(CATALOG_MAX_MILEAGE, value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleReset = () => {
+    setSelectedBrand('Enter the text');
+    setSelectedPrice('To $');
+    setMinMileage('');
+    setMaxMileage('');
+    setFilterMinMileage('');
+    setFilterMaxMileage('');
+
+    remove(CATALOG_BRAND);
+    remove(CATALOG_PRICE);
+    remove(CATALOG_MIN_MILEAGE);
+    remove(CATALOG_MAX_MILEAGE);
   };
 
   return (
     <>
-      <DropdownFilter onSubmit={handleSubmit} brands={getUniqueBrands(cars)} />
+      <DropdownMenu
+        brands={getUniqueBrands(cars)}
+        onFilterChange={handleChange}
+        onFilterReset={handleReset}
+        selectedBrand={selectedBrand}
+        selectedPrice={selectedPrice}
+        minMileage={minMileage}
+        maxMileage={maxMileage}
+      />
       {status === 'pending' && <Spinner />}
-      {status === 'rejected' && <h3>{error.message}</h3>}
+      {status === 'rejected' && <h3>{error?.message}</h3>}
       {status === 'resolved' && (
-        <CarList cars={cars} toggleFavorite={toggleFavorite} />
+        <CarList cars={filteredCars} toggleFavorite={toggleFavorite} />
       )}
       {page < 4 && (
         <LoadmoreBtn
