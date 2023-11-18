d543889e1477cce32bb7831fa03ce6091664ce90
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-11-18 22:10:11 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,13 @@
 export const FAVORITES_KEY = 'favorite-cars';
+
 export const SELECTED_BRAND = 'selected-brand';
 export const SELECTED_PRICE = 'selected-price';
 export const MIN_MILEAGE = 'min-mileage';
 export const MAX_MILEAGE = 'max-mileage';
+
+export const CATALOG_BRAND = 'catalog-brand';
+export const CATALOG_PRICE = 'catalog-price';
+export const CATALOG_MIN_MILEAGE = 'catalog-min-mileage';
+export const CATALOG_MAX_MILEAGE = 'catalog-max-mileage';
 
 export const prices = [30, 40, 50, 60, 70, 80, 90, 100];
