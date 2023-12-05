d53e3feea5855337634750612dc4de1f28b833ee
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-12-05 23:58:37 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -11,3 +11,9 @@ export const CATALOG_MIN_MILEAGE = 'catalog-min-mileage';
 export const CATALOG_MAX_MILEAGE = 'catalog-max-mileage';
 
 export const prices = [30, 40, 50, 60, 70, 80, 90, 100];
+
+export const Status = {
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
